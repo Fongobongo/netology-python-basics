e96cb0af92ec078cd5296b6712f559643584852b
Dmitry Byankin <eblosharik@yandex.ru>
2020-02-27 19:57:25 +0500

    homework 2.4 fix

--- 2.4/task_1_2.py
+++ 2.4/task_1_2.py
@@ -10,7 +10,7 @@ def add_dish(dish_name):
         ingredient_dict = {}
         ingredient = file.readline().split("|")
         ingredient_dict["ingredient_name"] = ingredient[0].strip()
-        ingredient_dict["quantity"] = ingredient[1].strip()
+        ingredient_dict["quantity"] = int(ingredient[1].strip())
         ingredient_dict["measure"] = ingredient[2].strip()
         ingredients_list.append(ingredient_dict)
 
@@ -36,14 +36,15 @@ def get_shop_list_by_dishes(dishes, person_count):
 
     for dish in dishes:
         for ingredient in cook_book.get(dish):
-            new_ingredient = ingredient.pop('ingredient_name')
-            if all_ingredients.get(new_ingredient) is None:
-                all_ingredients[new_ingredient] = ingredient
+            current_ingredient = ingredient.copy()
+            ingredient_name = current_ingredient.pop('ingredient_name')
+            if all_ingredients.get(ingredient_name) is None:
+                all_ingredients[ingredient_name] = current_ingredient
+                all_ingredients[ingredient_name]['quantity'] *= person_count
             else:
-                all_ingredients[new_ingredient]['quantity'] = int(all_ingredients[new_ingredient]['quantity']) + int(ingredient['quantity'])
-            all_ingredients[new_ingredient]['quantity'] = int(all_ingredients[new_ingredient]['quantity']) * person_count
+                all_ingredients[ingredient_name]['quantity'] += current_ingredient['quantity'] * person_count
 
     return all_ingredients
 
 
-print("Словарь с названием ингредиентов и его количества для блюда:", get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
+print("Словарь с названием ингредиентов и его количества для блюда:", get_shop_list_by_dishes(['Омлет', 'Омлет'], 2))
